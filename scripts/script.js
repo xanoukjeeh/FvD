c1cc073e96e7fb5f68fba50856073851351e60bc
xanoukjeeh <151958173+xanoukjeeh@users.noreply.github.com>
2024-12-05 15:11:49 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,13 @@
-// JavaScript Document
-console.log("hi");
+const hamburgerBtn = document.querySelector("header nav button:first-of-type");
+const navEL = document.querySelector("main > ul");
+
+console.log(hamburgerBtn);  // Debugging
+console.log(navEL);  // Debugging
+
+if (hamburgerBtn) {
+    hamburgerBtn.addEventListener("click", openMenu);
+}
+
+function openMenu() {
+    navEL.classList.toggle("active");
+}
